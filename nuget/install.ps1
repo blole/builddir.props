7ed63a190855cca2fbdb63f6c2f3ef2628e95f30
Björn Holm <bjh@kth.se>
2016-01-29 04:46:07 +0100

    using "manage-property-sheets"

--- nuget/install.ps1
+++ nuget/install.ps1
@@ -1,34 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
-. $toolsPath\funcs.ps1
+$pspkg = $(ls "$installPath\..\manage-property-sheets*")[-1]
+. "$pspkg\tools\manage-property-sheets.ps1"
 
-$projectName = $project.Name
-$projectFullName = $project.FullName
-$projectDir = $project.Properties.Item("ProjectDirectory").Value
-$builddirRelPath = Relative-Path $projectDir "$toolsPath\builddir.props"
-
-
-Select-Project $projectName
-$dte.ExecuteCommand("Project.UnloadProject")
-$projectMSBuild = [Microsoft.Build.Construction.ProjectRootElement]::Open($projectFullName)
-
-
-
-# The goal is to add something like this to the projects' .vcxproj:
-#
-# <ImportGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'" Label="PropertySheets">
-#+  <Import Project="..\packages\builddir.x.y.z\tools\builddir.props" Condition="exists('..\packages\builddir.x.y.z\tools\builddir.props')" />
-#   ...
-# </ImportGroup>
-
-foreach ($propertySheetGroup in $projectMSBuild.ImportGroups | where {$_.Label -eq "PropertySheets"})
-{
-	$import = $projectMSBuild.CreateImportElement($builddirRelPath);
-	$import.Condition = "exists('"+$builddirRelPath+"')"
-	$propertySheetGroup.PrependChild($import)
-}
-
-
-
-$projectMSBuild.Save()
-Select-Project $projectName
-$dte.ExecuteCommand("Project.ReloadProject")
+Add-Property-Sheet $project "$toolsPath\builddir.props"
